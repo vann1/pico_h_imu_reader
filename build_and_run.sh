45604e7d5b3fc0c5a34ac74e8d65dc20e4fe2700
EmilFrisk1 <vichy9876@gmail.com>
2025-08-04 11:08:12 +0300

    ?

---
+++ build_and_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e 
+
+echo "Configuring cmake..."
+cmake -S . -B build 
+echo "Entering build directory..."
+cd build
+
+echo "Building project..."
+make
+
+echo "Build completed successufully"
+
+cd ..

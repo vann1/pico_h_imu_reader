a54d6ab170df9a2d622ff4af1b0be7e11faf755a
vann1 <ollijk133@gmail.com>
2025-08-12 10:09:36 +0300

    removed unneccessary stuff

--- main.c
+++ main.c
@@ -64,7 +64,8 @@ void read_all_sensors(Sensor* sensors) {
     int index = 0;
    for (int i = 0; i < CHANNEL_COUNT; i++) {
         if(i == 0) {
-            bno055_read_reg(I2C_PORT_0,0x4A, &sensors[index].magnetometer);
+            ism330dhcx_read_accelerometer(I2C_PORT_0,ISM330DHCX_ADDR_DO_LOW, &sensors[index].accelerometer);
+            ism330dhcx_read_gyro(I2C_PORT_0,ISM330DHCX_ADDR_DO_LOW, &sensors[index].gyroscope);
             sensors[index].timestamp = clock();
             index++;
             ism330dhcx_read_accelerometer(I2C_PORT_0,ISM330DHCX_ADDR_DO_HIGH, &sensors[index].accelerometer);

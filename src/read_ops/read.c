a46cfc77647abdcc10e057164b3cb47f79c941f2
EmilFrisk1 <vichy9876@gmail.com>
2025-08-18 12:32:56 +0300

    ?

--- src/read_ops/read.c
+++ src/read_ops/read.c
@@ -8,6 +8,9 @@ void read_all_sensors(Sensor* sensors) {
             ism330dhcx_read_gyro(I2C_PORT_0,ISM330DHCX_ADDR_DO_LOW, &sensors[index].gyroscope);
             sensors[index].timestamp = clock();
             index++;
+            ism330dhcx_read_accelerometer(I2C_PORT_0,ISM330DHCX_ADDR_DO_HIGH, &sensors[index].accelerometer);
+            ism330dhcx_read_gyro(I2C_PORT_0,ISM330DHCX_ADDR_DO_HIGH, &sensors[index].gyroscope);
+            sensors[index].timestamp = clock();
         } else if (i == 1) {
             ism330dhcx_read_accelerometer(I2C_PORT_0,ISM330DHCX_ADDR_DO_HIGH, &sensors[index].accelerometer);
             ism330dhcx_read_gyro(I2C_PORT_0,ISM330DHCX_ADDR_DO_HIGH, &sensors[index].gyroscope);

e54c1b9f6ed3b282cece5ba1ae8e38e2948772d9
EmilFrisk1 <vichy9876@gmail.com>
2025-08-19 12:40:45 +0300

    added benchmakrs

--- main.c
+++ main.c
@@ -43,13 +43,19 @@ int main() {
     
     int counter = 0;
     clock_t start_time = clock();
+    clock_t elapsed_time = start_time;
     while (true) {
+        // uint64_t start = time_us_64();
         read_all_sensors(sensors);
+            // uint64_t end = time_us_64();
+        // printf("READ ALL SENSORS ------ %llu\n", end-start);
+        
+        // uint64_t start = time_us_64();
         for (int i=0; i<SENSOR_COUNT;i++) {
             sensors[i].gyroscope = FusionCalibrationInertial(sensors[i].gyroscope, sensors[i].calibration.gyroscopeMisalignment, sensors[i].calibration.gyroscopeSensitivity, sensors[i].calibration.gyroscopeOffset);
             sensors[i].accelerometer = FusionCalibrationInertial(sensors[i].accelerometer, sensors[i].calibration.accelerometerMisalignment, sensors[i].calibration.accelerometerSensitivity, sensors[i].calibration.accelerometerOffset);
             sensors[i].gyroscope = FusionOffsetUpdate(&sensors[i].offset, sensors[i].gyroscope);
-
+            
             const float deltaTime = (float) (sensors[i].timestamp - sensors[i].previousTimestamp) / (float) CLOCKS_PER_SEC;
             sensors[i].previousTimestamp = sensors[i].timestamp;
 
@@ -60,11 +66,23 @@ int main() {
             sensors_data[i][2] = quat.element.y;
             sensors_data[i][3] = quat.element.z;           
         }
+        // uint64_t end = time_us_64();
+        // printf("FUSION AHRS UPDATE ------ %llu\n", end-start);
+        // uint64_t start = time_us_64();
         print_output_data();
-
+        // uint64_t end = time_us_64();
+        // printf("WHOLE THINGY ------ %llu\n", end-start);
         
+        counter++;
+        elapsed_time = (clock() - start_time) / (float)CLOCKS_PER_SEC;
+        if (elapsed_time >= 1) {
+            printf("\n\n\nCounter ------------- %d \n\n\n", counter);
+            start_time = clock();
+            counter = 0;
+        }
 
-        sleep_ms(SLEEP_DURATION((float)SAMPLE_RATE));
+        // sleep_ms(SLEEP_DURATION((float)SAMPLE_RATE));
+        sleep_ms(2.1); // Hardcoded sleep to achieve 120 hz sample rate for this setup
     }
     return 0;
 }
--- pipeline.sh
+++ pipeline.sh
@@ -4,7 +4,7 @@ pLoc=.venv/bin/python
 sReader=usb_serial_reader.py
 
 source build_and_run.sh
-sleep 10
+sleep 5
 sudo $pLoc $sReader asdasd
 sleep 5
 source transfer_uf2_topico_linux.sh

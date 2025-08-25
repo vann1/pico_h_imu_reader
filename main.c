452bb290dbbdf13e9c8285ff4bde702fd6692365
EmilFrisk1 <vichy9876@gmail.com>
2025-08-25 18:25:04 +0300

    Made benchmark

--- main.c
+++ main.c
@@ -6,25 +6,62 @@
 #include <stdio.h>
 #include <time.h>
 #include <sh2_paketti.h>
+#include <math.h>
 #include "read.h"
 extern sh2_vector_list_t sh2_vector_list;
 
 #define SLEEP_DURATION(hz) (float)(1.0f/hz * 1000.0f)
-#define ARRAY_SIZE (1024)
-#define RESULT_COUNT (64)
+#define SAMPLE_COUNT (1024)
+#define RESULT_COUNT (10)
 
 float sensors_data[SENSOR_COUNT][4];
 
-
 typedef struct data_fluctuation_t {
-    float gyro[3][ARRAY_SIZE];
-    float gyro[3][ARRAY_SIZE];
-    float results[2][RESULT_COUNT];
-    int cursor;
+    float gyro[SAMPLE_COUNT][3];
+    float accel[SAMPLE_COUNT][3];
+    float results[RESULT_COUNT][2][3];
 } data_fluctuation_t;
 
-data_fluctuation_t benchmark = {.cursor = 0};
+data_fluctuation_t benchmark;
 
+void calculate_results_avg(float results[][2][3], float answers[][3]) {
+    float avgs[2][3];
+    for (int i = 0; i < RESULT_COUNT; i++) {
+        // gyro
+        avgs[0][0] += results[i][0][0];
+        avgs[0][1] += results[i][0][1];
+        avgs[0][2] += results[i][0][2];
+        //Accel
+        avgs[1][0] += results[i][1][0];
+        avgs[1][1] += results[i][1][1];
+        avgs[1][2] += results[i][1][2];
+    }
+    answers[0][0] = avgs[0][0] / (float)RESULT_COUNT;
+    answers[0][1] = avgs[0][1] / (float)RESULT_COUNT;
+    answers[0][2] = avgs[0][2] / (float)RESULT_COUNT;
+
+    answers[1][0] = avgs[1][0] / (float)RESULT_COUNT;
+    answers[1][1] = avgs[1][1] / (float)RESULT_COUNT;
+    answers[1][2] = avgs[1][2] / (float)RESULT_COUNT;
+}
+
+void print_avg_fluctuations(float results[][3]) {
+    printf("Avg fluctuations for a gyroscope and accelerometer from a pool of %d results and each containing %d sample counts\n", RESULT_COUNT, SAMPLE_COUNT);
+    printf("Gyroscope: x: %.4f; y: %.4f; z: %.4f\n", results[0][0], results[0][1], results[0][2]);
+    printf("Accelerometer: x: %.4f; y: %.4f; z: %.4f\n", results[1][0], results[1][1], results[1][2]);
+}
+
+void calculate_avg_fluctuation(float gyro[][3], float* result) {
+    float avg_diff_sum[3];
+    for (int i = 0; i < SAMPLE_COUNT-1; i++) {
+        avg_diff_sum[0] += fabs(gyro[i][0] - gyro[i+1][0]);
+        avg_diff_sum[1] += fabs(gyro[i][1] - gyro[i+1][1]);
+        avg_diff_sum[2] += fabs(gyro[i][2] - gyro[i+1][2]);
+    }
+    result[0] = avg_diff_sum[0] / (float)(SAMPLE_COUNT-1);
+    result[1] = avg_diff_sum[1] / (float)(SAMPLE_COUNT-1);
+    result[2] = avg_diff_sum[2] / (float)(SAMPLE_COUNT-1);
+}
 
 void print_output_data (void) {
     for (int i = 0; i < SENSOR_COUNT; i++) {
@@ -60,28 +97,39 @@ int main() {
     initialize_algos(sensors);   
     
     int counter = 0;
-    uint64_t start_time = time_us_64();
-    while (true) {
-        read_all_sensors(sensors);
-        // for (int i=0; i<SENSOR_COUNT;i++) {
-        //     sensors[i].gyroscope = FusionCalibrationInertial(sensors[i].gyroscope, sensors[i].calibration.gyroscopeMisalignment, sensors[i].calibration.gyroscopeSensitivity, sensors[i].calibration.gyroscopeOffset);
-        //     sensors[i].accelerometer = FusionCalibrationInertial(sensors[i].accelerometer, sensors[i].calibration.accelerometerMisalignment, sensors[i].calibration.accelerometerSensitivity, sensors[i].calibration.accelerometerOffset);
-        //     sensors[i].gyroscope = FusionOffsetUpdate(&sensors[i].offset, sensors[i].gyroscope);
+    for (int i = 0; i<RESULT_COUNT;i++)
+    {
+        for (int j = 0; i<SAMPLE_COUNT;j++) {
+            read_all_sensors(sensors);
+            benchmark.gyro[j][0] = sensors[0].gyroscope.axis.x;
+            benchmark.gyro[j][1] = sensors[0].gyroscope.axis.y;
+            benchmark.gyro[j][2] = sensors[0].gyroscope.axis.z;
 
-        //     const float deltaTime = (float) (sensors[i].timestamp - sensors[i].previousTimestamp) / 1e6f;
-        //     sensors[i].previousTimestamp = sensors[i].timestamp;
-        //     // FusionAhrsUpdateNoMagnetometer(&sensors[i].ahrs, sensors[i].gyroscope, sensors[i].accelerometer, deltaTime);
+            benchmark.accel[j][0] = sensors[0].accelerometer.axis.x;
+            benchmark.accel[j][1] = sensors[0].accelerometer.axis.y;
+            benchmark.accel[j][2] = sensors[0].accelerometer.axis.z;
+            sleep_ms(1); // 120hz | 2
+        }
 
-        //     FusionAhrsUpdateExternalHeading(&sensors[i].ahrs, sensors[i].gyroscope, sensors[i].accelerometer, 0.0f, deltaTime);
-        //     const FusionQuaternion quat = FusionAhrsGetQuaternion(&sensors[i].ahrs);
-        //     sensors_data[i][0] = quat.element.w;
-        //     sensors_data[i][1] = quat.element.x;
-        //     sensors_data[i][2] = quat.element.y;
-        //     sensors_data[i][3] = quat.element.z;           
-        // }
-        // print_output_data();
-        print_raw_sensor_data(sensors);
-        sleep_ms(100); // 120hz | 2
+        float gyro_results[3];
+        float accel_results[3];
+        calculate_avg_fluctuation(&benchmark.gyro, &gyro_results);
+        calculate_avg_fluctuation(&benchmark.accel, &accel_results);
+
+        //Gyro
+        benchmark.results[i][0][0] = gyro_results[0];
+        benchmark.results[i][0][1] = gyro_results[1];
+        benchmark.results[i][0][2] = gyro_results[2];
+
+        //accel
+        benchmark.results[i][1][0] = accel_results[0];
+        benchmark.results[i][1][1] = accel_results[1];
+        benchmark.results[i][1][2] = accel_results[2];
     }
+    float final_answers[2][3];
+    calculate_results_avg(&benchmark.results, &final_answers);
+    print_avg_fluctuations(final_answers);
+
+    while(1);
     return 0;
 }

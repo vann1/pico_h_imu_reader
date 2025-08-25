6953085c0f4199245b8eab7b4ee05d4b15543868
vann1 <ollijk133@gmail.com>
2025-08-25 11:35:30 +0300

    changes

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ extern sh2_vector_list_t sh2_vector_list;
 
 #define SLEEP_DURATION(hz) (float)(1.0f/hz * 1000.0f)
 
-float sensors_data[SENSOR_COUNT+1][4];
+float sensors_data[SENSOR_COUNT][4];
 
 
 void print_output_data (void) {
-    for (int i = 0; i < SENSOR_COUNT+1; i++) {
-        printf("w%d: %.2f, x%d: %.2f, y%d: %.2f, z%d: %.2f | ",i, sensors_data[i][0],i, sensors_data[i][1],i, sensors_data[i][2],i, sensors_data[i][3] );
+    for (int i = 0; i < SENSOR_COUNT; i++) {
+        printf("w%d: %.4f, x%d: %.4f, y%d: %.4f, z%d: %.4f | ",i, sensors_data[i][0],i, sensors_data[i][1],i, sensors_data[i][2],i, sensors_data[i][3] );
     }
     printf("\n");
 }
@@ -27,7 +27,7 @@ int main() {
     while (!tud_cdc_connected()) {
         sleep_ms(100);
     }
-    setup_sh2_service();
+    // setup_sh2_service();
     int result = setup_I2C_pins();
 
     if (result != 1) {
@@ -43,7 +43,6 @@ int main() {
     
     int counter = 0;
     clock_t start_time = clock();
-
     while (true) {
         read_all_sensors(sensors);
         for (int i=0; i<SENSOR_COUNT;i++) {
@@ -53,38 +52,17 @@ int main() {
 
             const float deltaTime = (float) (sensors[i].timestamp - sensors[i].previousTimestamp) / (float) CLOCKS_PER_SEC;
             sensors[i].previousTimestamp = sensors[i].timestamp;
+            // FusionAhrsUpdateNoMagnetometer(&sensors[i].ahrs, sensors[i].gyroscope, sensors[i].accelerometer, deltaTime);
 
-            FusionAhrsUpdateNoMagnetometer(&sensors[i].ahrs, sensors[i].gyroscope, sensors[i].accelerometer, deltaTime);
+            FusionAhrsUpdateExternalHeading(&sensors[i].ahrs, sensors[i].gyroscope, sensors[i].accelerometer, 0.0f, deltaTime);
             const FusionQuaternion quat = FusionAhrsGetQuaternion(&sensors[i].ahrs);
             sensors_data[i][0] = quat.element.w;
             sensors_data[i][1] = quat.element.x;
             sensors_data[i][2] = quat.element.y;
             sensors_data[i][3] = quat.element.z;           
         }
-        read_super_sensor();
-        if (sh2_vector_list.data_ready == false) {
-            sleep_ms(1000);
-            printf("pitäisi tulostua kerran.");
-            sh2_vector_list.data_ready == true;
-        } 
-        else {
-            sensors_data[SENSOR_COUNT][0] = sh2_vector_list.rolling_list[sh2_vector_list.cursor][0];
-            sensors_data[SENSOR_COUNT][1] = sh2_vector_list.rolling_list[sh2_vector_list.cursor][1];
-            sensors_data[SENSOR_COUNT][2] = sh2_vector_list.rolling_list[sh2_vector_list.cursor][2];
-            sensors_data[SENSOR_COUNT][3] =sh2_vector_list.rolling_list[sh2_vector_list.cursor][3];
-            print_output_data();
-            printf("---\n");
-        }   
-        float elapsed_time = (clock() - start_time)/ (float) CLOCKS_PER_SEC;
-        counter++;
-
-        if (elapsed_time >= 1) {
-            printf("main.c counter: %d\n", counter);
-            counter = 0;
-            start_time = clock();
-        }
+        print_output_data();
         sleep_ms(SLEEP_DURATION((float)SAMPLE_RATE));
-
     }
     return 0;
 }

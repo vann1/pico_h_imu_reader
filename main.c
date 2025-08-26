c0c07d50ad7c9176daad58165b0bdf5a030666fa
vann1 <ollijk133@gmail.com>
2025-08-26 16:54:20 +0300

    .

--- main.c
+++ main.c
@@ -8,12 +8,120 @@
 #include <sh2_paketti.h>
 #include "read.h"
 #include "InitFusion.h"
+
+enum settings_enum_e {
+    S_SENSOR_COUNT,
+    S_LPF_ENABLED,
+    S_LPF_ALPHA,
+    S_SAMPLE_RATE
+};
+typedef enum settings_enum_e settings_enum;
+settings_enum settings_option;
+
+typedef struct imu_reader_settings_t {
+    int sensorCount;
+    int lpfEnabled;
+    float lpf_alpha;
+    int sampleRate;
+} imu_reader_settings_t;
+static imu_reader_settings_t imu_reader_settings = {0};
+
 extern sh2_vector_list_t sh2_vector_list;
 #define TIME_SLEEP (1000.0f/(float)SAMPLE_RATE)
 #define SLEEP_DURATION(hz) (float)(1.0f/hz * 1000.0f)
-
+#define SETTINGS_BUF_LEN (256)
 float sensors_data[SENSOR_COUNT][4];
-#define LPF_ALPHA 0.1f
+
+
+void extract_part(const char* part, const char* buf, settings_enum setting) {
+        char *pos = strstr(buf, part);
+
+        if (pos == NULL) {
+            printf("Error while extracting part %s not found in the buffer\n", part);
+            while(1);
+        } else {
+            printf("Found part %s!\n", part);
+            char part_buffer[64];
+            memset(part_buffer, 0, sizeof(part_buffer));
+            int cursor = 0;
+            int part_index = (pos - buf) + strlen(part);
+            for (int j=part_index;j<SETTINGS_BUF_LEN-part_index; j++) {
+                if (buf[j] != '|') {
+                    part_buffer[cursor++] = buf[j];
+                    continue;
+                }
+                part_buffer[cursor++] = '\0';
+                break;
+            }
+            printf("Part value extracted: %s\n", part_buffer);
+            switch (setting) {
+                case S_SENSOR_COUNT:
+                    imu_reader_settings.sensorCount = atoi(part_buffer);
+                    break;
+                case S_LPF_ENABLED:
+                    imu_reader_settings.lpfEnabled = atoi(part_buffer);
+                    break;
+                case S_LPF_ALPHA:
+                    sscanf(part_buffer, "%f",&imu_reader_settings.lpf_alpha);
+                    break;
+                case S_SAMPLE_RATE:
+                    imu_reader_settings.sampleRate = atoi(part_buffer);
+                    break;
+                default:
+                    printf("Error: Wrong setting %d\n", setting);
+                    while(1);
+            }
+            printf("Part %s found and saved: %s\n", part, part_buffer);
+        }
+}
+
+void excract_settings(const char* buf) {
+    settings_option = S_SENSOR_COUNT;
+    extract_part("SC=", buf, settings_option);
+
+    settings_option = S_LPF_ENABLED;
+    extract_part("LPF_ENABLED=", buf, settings_option);
+
+    settings_option = S_LPF_ALPHA;
+    extract_part("LPF_ALPHA=", buf, settings_option);
+
+    settings_option = S_SAMPLE_RATE;
+    printf("Starting to look for SR= part\n");
+    extract_part("SR=", buf, settings_option);
+
+    printf("Settings: Sensor Count: %d; LPF Enabled %d; LPF_ALPHA %f; Sample Rate: %d\n",imu_reader_settings.sensorCount, imu_reader_settings.lpfEnabled, imu_reader_settings.lpf_alpha, imu_reader_settings.sampleRate);
+}
+
+void set_settings() {
+    #undef SENSOR_COUNT
+    #define SENSOR_COUNT imu_reader_settings.sensorCount
+    #undef LPF_ENABLED
+    #define LPF_ENABLED imu_reader_settings.lpfEnabled
+    #undef LPF_ENABLED
+    #define LPF_ENABLED imu_reader_settings.lpf_alpha
+    #undef SAMPLE_RATE
+    #define SAMPLE_RATE imu_reader_settings.sampleRate
+}
+
+// Waits for the user to send in some settings through the serial port
+void wait_for_settings() {
+    char buf[SETTINGS_BUF_LEN];
+    while (1) {
+        if (tud_cdc_available()) {
+            uint32_t count = tud_cdc_read(buf, sizeof(buf)-1);
+            if (count > 0) {
+                buf[count] = '\0';
+                // Process input (e.g., echo back)
+
+                printf("Received: %s\n", buf);
+                excract_settings(buf);
+                set_settings();
+                break;
+            }
+        }
+        sleep_ms(1000); // Avoid tight loop
+    }
+}
 
 
 void print_output_data (void) {
@@ -24,7 +132,7 @@ void print_output_data (void) {
 }
 
 float apply_lpf(float new_val, float old_value) {
-    return old_value*(1.0f-LPF_ALPHA) + LPF_ALPHA*new_val;
+    return old_value*(1.0f-imu_reader_settings.lpf_alpha) + imu_reader_settings.lpf_alpha*new_val;
 }
 
 
@@ -33,6 +141,7 @@ int main() {
     while (!tud_cdc_connected()) {
         sleep_ms(100);
     }
+    wait_for_settings();
     // setup_sh2_service();
     int result = setup_I2C_pins();
 
